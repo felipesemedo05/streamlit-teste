0e6fba0da8b1846d2a3e96ce92603bd05869a44f
felipesemedo05 <felipe.semedo@eletromidia.com.br>
2024-09-03 13:20:49 -0300

    teste

--- app.py
+++ app.py
@@ -356,6 +356,7 @@ if uploaded_file is not None:
                                 (df['num_total_impressions'].isnull()) & 
                                 (df['home'].isnull()))]
                 df_data_filtrado = df_data[['location_id', 'impressions', 'uniques', 'date']]
+                df_data_filtrado['date'] = pd.to_datetime(df_data_filtrado['date'])
                 st.dataframe(df_data_filtrado)          
     except Exception as e:
         st.error(f"Ocorreu um erro ao processar o arquivo: {e}")
