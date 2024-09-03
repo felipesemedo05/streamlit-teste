68ff629705b08311bc58692c57e80689d8cfcad0
felipesemedo05 <felipe.semedo@eletromidia.com.br>
2024-09-03 10:05:38 -0300

    teste

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ st.set_page_config(page_title='Processamento de Arquivo', layout='wide')
 # Título
 st.title('Processamento de Análise da Claro')
 
-col1, col4 = st.columns(2)
-with col4:
+col1, col2 = st.columns([2, 1])
+with col2:
     st.image("eletro.png", width=300)
 
 # Conteúdo restant
